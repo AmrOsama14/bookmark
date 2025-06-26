75e790a3f44d4a2f34f9e0d4a450278f8cf7a1dd
AmrOsama14 <amrusama69@gmail.com>
2025-06-26 13:51:17 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -27,19 +27,23 @@ function handleDisplaySite() {
     temp += `<tr>
           <td>${i + 1}</td>
           <td>${webSiteList[i].name}</td>
-          <td><button class="btn btn-visit"><a href="https://${webSiteList[i].url}" target="blank"><i class="fa-solid fa-eye"></i> Visit</a></button></td>
-          <td><button class="btn btn-delete " onclick="handleDeleteSite(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button></td>
+          <td><button class="btn btn-visit" onclick="VisitWebsite('${webSiteList[i].url}')"> <i class="fa-solid fa-eye"></i> Visit</button></td>
+          <td><button class="btn btn-delete" onclick="handleDeleteSite(${i})"> <i class="fa-solid fa-trash-can"></i> Delete</button></td>
         </tr>`;
   }
   document.getElementById("tableData").innerHTML = temp;
 }
 
 //<td><button class="btn btn-visit"><a href="https://${webSiteList[i].url}" target="blank"><i class="fa-solid fa-eye"></i> Visit</a></button></td>
-//<td><button class="btn btn-visit" onclick="VisitWebsite(${webSiteList[i].url})"><i class="fa-solid fa-eye"></i> Visit</button></td>
+//<td><button class="btn btn-visit" onclick="VisitWebsite('${webSiteList[i].url}')"><i class="fa-solid fa-eye"></i> Visit</button></td>
 
-// function VisitWebsite (url) {
-//   window.open(url , '_blank')
-// }
+function VisitWebsite (url) {
+  if (url.includes("https://") || url.includes("http://")){
+    window.open(url , "_blank");
+  } else {
+    window.open('https://' + url , "_blank");
+  }
+}
 
 function handleDeleteSite(index) {
   webSiteList.splice(index, 1);
@@ -69,7 +73,7 @@ function validName () {
 }
 
 function validUrl (){
-  let regexUrl= /(https:\/\/)?(www\.)[a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
+  let regexUrl= /(https:\/\/|http:\/\/)?(www\.)[a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
   if(regexUrl.test(siteUrlInput.value)) {
     siteUrlInput.classList.add("is-valid");
     siteUrlInput.classList.remove("is-invalid");
